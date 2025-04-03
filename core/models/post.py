4bc048787ae6b4b399297e98051871ddbdc5dff8
NAKAZUKA <mishin23042002@gmail.com>
2025-04-03 16:16:10 +0300

    create

--- core/models/post.py
+++ core/models/post.py
@@ -1,16 +1,12 @@
-from typing import TYPE_CHECKING
-
-from sqlalchemy import String, Text, ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import String, Text
+from sqlalchemy.orm import Mapped, mapped_column
 
 from .base import Base
+from .mixins import UserRelationMixin
 
 
-if TYPE_CHECKING:
-    from .user import User
-
-
-class Post(Base):
+class Post(UserRelationMixin, Base):
+    _user_back_populates = "posts"
 
     title: Mapped[str] = mapped_column(String(64))
     body: Mapped[str] = mapped_column(
@@ -18,7 +14,3 @@ class Post(Base):
         default="",
         server_default="",
     )
-    user_id: Mapped[int] = mapped_column(
-        ForeignKey("users.id"),
-    )
-    user: Mapped["User"] = relationship(back_populates="posts")
